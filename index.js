5d5c40b7a5f0b4c2389b190db9284dd3fb718df2
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-01-12 23:51:15 +0000

    Refactored code, added JSDoc and comments

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Dev dependencies
 const dotenv = require("dotenv");
 const chalk = require("chalk");
+
 // Dependencies
 const { Octokit } = require("@octokit/core");
 
+// Destructuring to get specific chalk colours
 const { red, yellow, green } = chalk;
 
-console.log(yellow("Hello, World!"));
+// Load environment variables from .env file
 dotenv.config();
 
-/**
- * Ensure that the API_KEY variable is provided in the .env file
- */
+// Ensure that the API_KEY variable is provided in the .env file
 if (!process.env.API_KEY) {
     console.error(red("Please provide a API_KEY variable in the .env file."));
     process.exit(1);
@@ -21,15 +21,15 @@ if (!process.env.API_KEY) {
  * The API key for GitHub
  * @type {number}
  */
-const apiKey = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
+const USERNAME = "Karl-Horning";
+const REPO_API_ROUTE = "GET /users/{username}/repos?per_page=100&page=1";
+const TAG_API_ROUTE = "GET /repos/{owner}/{repo}/topics";
 
-/**
- * Handle SIGINT for graceful shutdown.
- */
+// Handle SIGINT for graceful shutdown.
 process.on("SIGINT", async () => {
     try {
         console.log(yellow("Received SIGINT. Closing Node.js app..."));
-        await server.stop();
         console.log(green("Node.js app closed."));
         process.exit(0);
     } catch (error) {
@@ -38,51 +38,119 @@ process.on("SIGINT", async () => {
     }
 });
 
-// Octokit.js
-// https://github.com/octokit/core.js#readme
+// Ensure that the API_KEY variable is provided in the .env file
+if (!API_KEY || API_KEY.trim() === "") {
+    console.error(
+        red("Please provide a valid API_KEY variable in the .env file.")
+    );
+    process.exit(1);
+}
+
+/**
+ * The API key for GitHub
+ * @type {number}
+ */
+const apiKey = API_KEY;
+
 const octokit = new Octokit({
     auth: apiKey,
 });
 
-const getAllRepos = async () => {
+/**
+ * Fetches repository information for a GitHub user.
+ *
+ * @param {string} [username=USERNAME] - The GitHub username for which to fetch repository information.
+ * @throws {Error} Throws an error if the provided username is invalid or if there's an issue fetching the repository information.
+ * @returns {Promise<Array>} A promise that resolves to an array containing repository information for the specified user.
+ */
+const getAllRepoInformation = async (username = USERNAME) => {
+    // Input validation
+    if (typeof username !== "string" || username.trim() === "") {
+        throw new Error("Invalid username");
+    }
+
     try {
-        const response = await octokit.request("GET /users/{username}/repos", {
-            username: "Karl-Horning",
-            headers: {
-                "X-GitHub-Api-Version": "2022-11-28",
-            },
+        // API request to fetch repo information
+        const response = await octokit.request(REPO_API_ROUTE, {
+            username,
         });
 
-        return response;
-    } catch (error) {
-        return "Error:", error;
-    }
-};
-
-const getRepoTags = async () => {
-    try {
-        const response = await octokit.request(
-            "GET /repos/{owner}/{repo}/topics",
-            {
-                owner: "Karl-Horning",
-                repo: "change-string-case",
-                headers: {
-                    "X-GitHub-Api-Version": "2022-11-28",
-                },
-            }
+        // Log the successful response
+        console.log(
+            green(`Successfully fetched repo information for ${username}`)
         );
 
-        return response;
+        return response.data;
     } catch (error) {
-        console.error("Error:", error);
+        // Log the error
+        console.error(red(`Error fetching repo information: ${error.message}`));
+
+        // Rethrow the error with a more descriptive message
+        throw new Error(
+            `Error fetching repo information for ${username}: ${error.message}`
+        );
     }
 };
 
-const logResponse = async () => {
-    // const allRepos = await getAllRepos();
-    const repoTags = await getRepoTags();
+/**
+ * Extracts repo names from an array of repositories.
+ *
+ * @param {Array} repos - An array of repository objects.
+ * @returns {Array} An array of repository names.
+ */
+const getRepoNames = (repos) => repos.map((repo) => repo.name);
 
-    console.log(repoTags.data.names);
+/**
+ * Fetches tags for a specific repository.
+ *
+ * @param {string} repo - The name of the repository.
+ * @throws {Error} Throws an error if there's an issue fetching tag information.
+ * @returns {Promise<Array>} A promise that resolves to an array containing tag names for the specified repository.
+ */
+const getRepoTags = async (repo) => {
+    try {
+        const response = await octokit.request(TAG_API_ROUTE, {
+            owner: USERNAME,
+            repo,
+        });
+
+        // Log the successful response
+        console.log(green(`Successfully fetched ${repo}`));
+
+        return response.data.names;
+    } catch (error) {
+        // Log the error
+        console.error(red(`Error fetching tag information: ${error.message}`));
+
+        // Rethrow the error with a more descriptive message
+        throw new Error(
+            `Error fetching tags for repo ${repo}: ${error.message}`
+        );
+    }
+};
+
+/**
+ * Fetches tags for multiple repositories in parallel.
+ *
+ * @param {Array} repoNames - An array of repository names.
+ * @returns {Promise<Array>} A promise that resolves to an array containing tag names for multiple repositories.
+ */
+const getAllRepoTags = async (repoNames) =>
+    Promise.all(repoNames.map(getRepoTags));
+
+/**
+ * Logs the response containing formatted repository tags.
+ */
+const logResponse = async () => {
+    try {
+        const allRepos = await getAllRepoInformation();
+        const repoNames = getRepoNames(allRepos);
+        const allRepoTags = await getAllRepoTags(repoNames);
+        const formattedRepoTags = allRepoTags.flatMap((tags) => tags);
+        console.log(formattedRepoTags);
+    } catch (error) {
+        console.error(red(`Error in logResponse: ${error.message}`));
+    }
 };
 
 logResponse();
